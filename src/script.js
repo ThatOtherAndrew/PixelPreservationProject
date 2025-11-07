40d3e4867cf3cea9d56d8a6fdade90570e5c5a63
Andromeda <stroev.andrew@gmail.com>
2025-11-07 13:31:32 +0000

    Prettier!

--- src/script.js
+++ src/script.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
 /**
  * @param {Game} game
  * @returns {HTMLLIElement}
  */
 function buildGameEntry(game) {
-    const template = document.getElementById("game-entry");
+    const template = document.getElementById('game-entry');
     const clone = template.content.cloneNode(true);
 
-    clone.querySelector(".title").innerText = game.title;
-    clone.querySelector(".genre").innerText = game.genre;
-    clone.querySelector(".platform").innerText = game.platform;
+    clone.querySelector('.title').innerText = game.title;
+    clone.querySelector('.genre').innerText = game.genre;
+    clone.querySelector('.platform').innerText = game.platform;
 
-    return clone.querySelector("li");
+    return clone.querySelector('li');
 }
 
 function onPageLoad() {
-    const gamesList = document.getElementById("games");
+    const gamesList = document.getElementById('games');
     for (const game of games) {
         console.log(game);
         gamesList.appendChild(buildGameEntry(game));
